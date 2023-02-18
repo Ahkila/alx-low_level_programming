c93f1a5d5dd9f82221b9c41aaf7f15d0f2be281b
Ahkila <zvidix95@.com>
2023-02-18 01:10:54 -0800

    Positive anything is better than nagative nothing

---
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+/**
+ * main - Entry
+ * Return: Always 0
+ */
+int main(void)
+{
+	int n;
+	int a;
+
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+	a = n % 10;
+
+	if (n > s)
+	{
+		printf(Last digit of %d is % and is greater than 5\n", n, a);
+	}
+	else if (a == 0)
+	{
+		printf("Last digit of %d is &
